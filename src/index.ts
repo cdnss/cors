959ffa72ace04b1887b10cf44e9593aa3ef72b40
cdnss <32522595+cdnss@users.noreply.github.com>
2025-04-27 18:43:29 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,183 +1,51 @@
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  ANALYTICS: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-}
+/**
+ * @license
+ * Copyright 2024 Google Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+const puppeteer = require('puppeteer');
 
-export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    const reqHeaders = new Headers(request.headers);
-    const response: {
-      body: BodyInit | null;
-      contentType: string | null;
-      status: number;
-      text: string;
-      headers: Headers;
-    } = {
-      body: null,
-      contentType: null,
-      status: 200,
-      text: "OK",
-      headers: new Headers({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods":
-          "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":
-          reqHeaders.get("Access-Control-Allow-Headers") ||
-          "Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With, Token, x-access-token",
-      }),
-    };
+/**
+ * To have Puppeteer fetch a Firefox binary for you, first run:
+ *
+ * PUPPETEER_PRODUCT=firefox npm install
+ *
+ * To get additional logging about which browser binary is executed,
+ * run this example as:
+ *
+ * DEBUG=puppeteer:launcher NODE_PATH=../ node examples/cross-browser.js
+ *
+ * You can set a custom binary with the `executablePath` launcher option.
+ */
 
-    try {
-      // get rid of https://
-      let url = request.url.substring(8);
-      // decode the original request url
-      url = decodeURIComponent(url.substring(url.indexOf("/") + 1));
-
-      if (
-        request.method == "OPTIONS" ||
-        url.length < 3 ||
-        url.indexOf(".") == -1 ||
-        url == "favicon.ico" ||
-        url == "robots.txt"
-      ) {
-        const invalid = !(request.method == "OPTIONS" || url.length === 0);
-        response.body = await getHelp(env, new URL(request.url));
-        response.contentType = "text/html";
-        response.status = invalid ? 400 : 200;
-      } else {
-        url = fixUrl(url);
-
-        let fetchRequest: {
-          headers: Headers;
-          method: string;
-          body?: BodyInit;
-        } = {
-          method: request.method,
-          headers: new Headers(),
-        };
-
-        const dropHeaders = ["content-length", "content-type", "host"];
-        for (let [key, value] of reqHeaders.entries()) {
-          if (!dropHeaders.includes(key)) {
-            fetchRequest.headers.set(key, value);
-          }
-        }
-
-        if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) >= 0) {
-          const ct = (reqHeaders.get("content-type") || "").toLowerCase();
-          if (ct.includes("application/json")) {
-            fetchRequest.body = JSON.stringify(await request.json());
-          } else if (
-            ct.includes("application/text") ||
-            ct.includes("text/html")
-          ) {
-            fetchRequest.body = await request.text();
-          } else if (ct.includes("form")) {
-            fetchRequest.body = await request.formData();
-          } else {
-            fetchRequest.body = await request.blob();
-          }
-        }
-
-        let fetchResponse = await fetch(url, fetchRequest);
-        response.contentType = fetchResponse.headers.get("content-type");
-        response.status = fetchResponse.status;
-        response.text = fetchResponse.statusText;
-        response.body = fetchResponse.body;
-
-        await increment(env);
-      }
-    } catch (err) {
-      if (err instanceof Error) {
-        response.contentType = "application/json";
-        response.body = JSON.stringify({
-          code: -1,
-          msg: JSON.stringify(err.stack) || err,
-        });
-        response.status = 500;
-      }
-    }
-
-    if (response.contentType && response.contentType != "") {
-      response.headers.set("content-type", response.contentType);
-    }
-
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.text,
-      headers: response.headers,
-    });
+const firefoxOptions = {
+  product: 'firefox',
+  extraPrefsFirefox: {
+    // Enable additional Firefox logging from its protocol implementation
+    // 'remote.log.level': 'Trace',
   },
+  // Make browser logs visible
+  dumpio: true,
 };
 
-function fixUrl(url: string) {
-  if (url.includes("://")) {
-    return url;
-  } else if (url.includes(":/")) {
-    return url.replace(":/", "://");
-  } else {
-    return "http://" + url;
-  }
-}
+(async () => {
+  const browser = await puppeteer.launch(firefoxOptions);
 
-async function getHelp(env: Env, url: URL) {
-  return `<!DOCTYPE html>
-  <html>
-	<head>
-	  <title>Streetwriters CORS Proxy</title>
-	  <style>
-		body {
-		  font-family: "Courier New", Courier, monospace;
-		}
-	  </style>
-	</head>
-	<body>
-	  <p>Welcome to Streetwriters CORS Proxy!</p>
-	  <p>
-		You can use this proxy to bypass CORS in the browser. It is recommended
-		that you deploy your own instance both for privacy reasons and to reduce
-		the load on a single instance.
-	  </p>
-	  <p>USAGE:</p>
-	  <p>
-		&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${url.hostname}/&lt;url-to-resource&gt;
-	  </p>
-	  <p>EXAMPLES:</p>
-	  <p>&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${
-    url.hostname
-  }/https://api.github.com/repos/streetwriters/notesnook/releases/tags/v2.2.4</p>
-	  <p>&nbsp;&nbsp;&nbsp;&nbsp;${url.protocol}//${
-    url.hostname
-  }/https://api.github.com/repos/streetwriters/notesnook/releases</p>
-	  <p>
-		<a
-		  href="https://deploy.workers.cloudflare.com/?url=https://github.com/streetwriters/cors"
-		  >Deploy your own!</a
-		>
-		| <a href="https://github.com/streetwriters/cors">GitHub repository</a>
-	  </p>
-	  <hr/>
-	  <p>
-		Bypassed ${await totalRequests(env)} requests!
-	  </p>
-	</body>
-  </html>
-  `;
-}
+  const page = await browser.newPage();
+  console.log(await browser.version());
 
-async function increment(env: Env) {
-  if (!env.ANALYTICS) return;
-  let count = parseInt((await env.ANALYTICS.get("total_requests")) || "0");
-  await env.ANALYTICS.put("total_requests", (++count).toFixed());
-}
+  await page.goto('https://xtgem.com');
 
-async function totalRequests(env: Env) {
-  if (!env.ANALYTICS) return 0;
-  return await env.ANALYTICS.get("total_requests");
-}
+  // Extract articles from the page.
+  const resultsSelector = '.titleline > a';
+  const links = await page.evaluate(resultsSelector => {
+    const anchors = Array.from(document.querySelectorAll(resultsSelector));
+    return anchors.map(anchor => {
+      const title = anchor.textContent.trim();
+      return `${title} - ${anchor.href}`;
+    });
+  }, resultsSelector);
+  console.log(links.join('\n'));
+
+  await browser.close();
+})();
